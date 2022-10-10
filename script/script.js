3127803a30b3c97d3c2024358342b85fe9019ee2
mlietti1 <mcf.lietti@outlook.com>
2022-10-10 15:20:15 +0200

    js updated

--- script/script.js
+++ script/script.js
@@ -1,7 +1,8 @@
+const anno = 22
 const nomeUtente = prompt("What's your name?");
 const cognomeUtente = prompt("Hi " + nomeUtente + ", what's your last name?");
 const coloreUtente = prompt("Now tell me, what's your favorite color?");
-const pwdUtente = nomeUtente + cognomeUtente + coloreUtente;
+const pwdUtente = nomeUtente + cognomeUtente + coloreUtente + anno;
 
 document.getElementById('pwd').innerHTML += `
     Here's your Password: <br>
